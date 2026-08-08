d46305672356fb2732346a8d461abfa865d53c95
root <root@localhost>
2026-08-08 17:20:38 +0700

    Cloud Build Request - 2026-08-08 17:20:38

--- app/src/main/java/net/openvpn/openvpn/OpenVPNPrefs.java
+++ app/src/main/java/net/openvpn/openvpn/OpenVPNPrefs.java
@@ -1,14 +1,38 @@
 package net.openvpn.openvpn;
 
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
 
-public class OpenVPNPrefs extends PreferenceActivity {
-    public void onCreate(Bundle savedInstanceState) {
-        if (OpenVPNClientBase.themeSet) {
-            setTheme(OpenVPNClientBase.themeResId);
-        }
+public class OpenVPNPrefs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.preferences);
+
+        // ซ่อนแถบชื่อแอป
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+        if (getActionBar() != null) {
+            getActionBar().hide();
+        }
+
+        // ตั้งพื้นหลังให้เข้ากับแอป
+        getWindow().getDecorView().setBackgroundColor(0xFFF5F6F8);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(android.R.id.content, new SettingsFragment())
+                    .commit();
+        }
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.preferences, rootKey);
+        }
     }
 }
